2003026745c9901122de754df99e063f3844ed12
Sachin Pikle <sachin.pikle@oracle.com>
2021-01-06 21:58:54 +0530

    Added AJDB code

--- step-2/getBookById/src/main/java/com/example/fn/GetBookById.java
+++ step-2/getBookById/src/main/java/com/example/fn/GetBookById.java
@@ -1,35 +1,35 @@
 package com.example.fn;
 
+import java.util.Arrays;
+//import java.util.Map;
+
 import com.fnproject.fn.api.FnConfiguration;
-import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
 import com.fnproject.fn.api.OutputEvent;
 import com.fnproject.fn.api.RuntimeContext;
-
-import java.util.Arrays;
-import java.util.Map;
+import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
 
 public class GetBookById {
 
     private String APP_NAME;
     private String FN_NAME;
+    private BooksDatabaseImpl booksDB;
 
-    private Map<String,String> books = Map.of(
-        "1", "{ \"isbn\": \"1\", \"title\": \"Java - The Complete Reference\", \"author\": \"Herbert Schildt\" }", 
-        "2", "{ \"isbn\": \"2\", \"title\": \"Effective Java\", \"author\": \"Joshua Bloch\" }", 
-        "3", "{ \"isbn\": \"3\", \"title\": \"Learning Python\", \"author\": \"Mark Lutz\" }", 
-        "4", "{ \"isbn\": \"4\", \"title\": \"JavaScript: The Definitive Guide\", \"author\": \"David Flanagan\" }", 
-        "5", "{ \"isbn\": \"5\", \"title\": \"Oracle SOA Suite 12c Handbook\", \"author\": \"Lucas Jellema\" }"
-    );
+    // private Map<String,String> books = Map.of(
+    //     "1", "{ \"isbn\": \"1\", \"title\": \"Java - The Complete Reference\", \"author\": \"Herbert Schildt\" }", 
+    //     "2", "{ \"isbn\": \"2\", \"title\": \"Effective Java\", \"author\": \"Joshua Bloch\" }", 
+    //     "3", "{ \"isbn\": \"3\", \"title\": \"Learning Python\", \"author\": \"Mark Lutz\" }", 
+    //     "4", "{ \"isbn\": \"4\", \"title\": \"JavaScript: The Definitive Guide\", \"author\": \"David Flanagan\" }", 
+    //     "5", "{ \"isbn\": \"5\", \"title\": \"Oracle SOA Suite 12c Handbook\", \"author\": \"Lucas Jellema\" }"
+    // );
 
     @FnConfiguration
     public void setUp(RuntimeContext ctx) {
         APP_NAME = ctx.getConfigurationByKey("APP_NAME").orElse("NO-APP_NAME");
         FN_NAME = ctx.getConfigurationByKey("FN_NAME").orElse("NO-FN_NAME");
+        booksDB = new BooksDatabaseImpl();
     }    
     
     public OutputEvent handleRequest(HTTPGatewayContext hctx, RuntimeContext ctx) {
-//        String name = (input == null || input.isEmpty()) ? "GetBookById"  : input;
-
         System.out.printf("Inside App Name: %s | Function Name: %s | Language: Java \n", APP_NAME, FN_NAME); 
         System.out.printf("Request URL is: %s\n", hctx.getRequestURL());
         String[] strArray = hctx.getRequestURL().split("/");
@@ -38,7 +38,8 @@ public class GetBookById {
         String bookId = strArray[3];
         System.out.printf("BookId: %s\n", bookId); 
         
-        String book = books.get(bookId);
+        // String book = books.get(bookId);
+        String book = booksDB.getById(bookId);
         System.out.printf("Get Book: %s\n", book); 
 
         OutputEvent out = null;
