3b201ddf74ad53bfa15256d65faa4ffb1d4535ed
Sachin Pikle <sachin.pikle@oracle.com>
2021-01-08 11:31:37 +0530

    Greg's change to pull DB password from Secrets in Vault

--- step-3/README.md
+++ step-3/README.md
@@ -6,3 +6,8 @@ allow dynamic-group dg-compartment-sachin-pikle to manage objects in compartment
 fn config function gh-app books-etl-python ordsbaseurl "https://CHANGEME.adb.us-ashburn-1.oraclecloudapps.com/ords/"
 
 fn config function gh-app books-etl-python dbpwd <dbpwd>
+fn config function gh-app books-etl-python dbschema <dbschema>
+fn config function gh-app books-etl-python dbuser <dbuser>
+
+fn config function gh-app books-etl-python dbpwd_secretocid <dbpwd_secretocid>
+
--- step-3/books-ETL-python/func.py
+++ step-3/books-ETL-python/func.py
@@ -10,13 +10,25 @@ import json
 import oci
 import logging
 import requests
+import base64
 
 from fdk import response
+
+def get_secret(secret_ocid):
+    signer = oci.auth.signers.get_resource_principals_signer()
+    try:
+        client = oci.secrets.SecretsClient({}, signer=signer)
+        secret_content = client.get_secret_bundle(secret_ocid).data.secret_bundle_content.content.encode('utf-8')
+        decrypted_secret_content = base64.b64decode(secret_content).decode("utf-8")
+    except Exception as ex:
+        print("ERROR: failed to retrieve the secret content", ex, flush=True)
+        raise
+    return decrypted_secret_content
 
 # soda_insert uses the Autonomous Database REST API to insert JSON documents
 def soda_insert(ordsbaseurl, dbschema, dbuser, dbpwd, collection, logentries):
     auth=(dbuser, dbpwd)
-    sodaurl = ordsbaseurl + dbschema + '/soda/latest/'
+    sodaurl = ordsbaseurl + dbschema.lower() + '/soda/latest/'
     bulkinserturl = sodaurl + 'custom-actions/insert/' + collection + "/"
     headers = {'Content-Type': 'application/json'}
     resp = requests.post(bulkinserturl, auth=auth, headers=headers, data=json.dumps(logentries))
@@ -73,7 +85,7 @@ def handler(ctx, data: io.BytesIO=None):
     logger = logging.getLogger()
     logger.info("function start")
     signer = oci.auth.signers.get_resource_principals_signer()
-    object_name = bucket_name = namespace = ordsbaseurl = schema = dbuser = dbpwd = ""
+    object_name = bucket_name = namespace = ordsbaseurl = schema = dbuser = dbpwd_secretocid = ""
     try:
         cfg = ctx.Config()
         input_bucket = cfg["input-bucket"]
@@ -81,10 +93,10 @@ def handler(ctx, data: io.BytesIO=None):
         ordsbaseurl = cfg["ordsbaseurl"]
         schema = cfg["dbschema"]
         dbuser = cfg["dbuser"]
-        dbpwd = cfg["dbpwd"]
+        dbpwd_secretocid = cfg["dbpwd_secretocid"]
         collection = cfg["collection"]
     except Exception as e:
-        logger.error('Missing configuration keys: input-bucket, processed-bucket, ordsbaseurl, dbschema, dbuser, dbpwd and collection')
+        logger.error('Missing configuration keys: input-bucket, processed-bucket, ordsbaseurl, dbschema, dbuser, dbpwd_secretocid and collection')
         raise
 
     try:
@@ -100,6 +112,8 @@ def handler(ctx, data: io.BytesIO=None):
     except Exception as e:
         print('ERROR: bad Event!', flush=True)
         raise
+    print('INFO: retrieving secret ocid: ' + dbpwd_secretocid, flush=True)
+    dbpwd = get_secret(dbpwd_secretocid)
     load_data(signer, namespace, input_bucket, object_name, ordsbaseurl, schema, dbuser, dbpwd, collection)
     move_object(signer, namespace, input_bucket, processed_bucket, object_name)
 
